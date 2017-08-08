35b915ed5227204c5e148f705bf4a397277cdf64
Nathaniel Brion Sison <sison.nathanielbrion@outlook.com>
2017-08-09 02:46:51 +0800

    Added bootstrap template and added some dummy data

--- ngsison/Controllers/HomeController.cs
+++ ngsison/Controllers/HomeController.cs
@@ -10,25 +10,7 @@ namespace ngsison.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
+			ViewData["Title"] = "ngsison - Home";
             return View();
         }
     }
